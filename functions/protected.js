b469f0cd1863cd5a4c78c843683964ae004b5fe0
Travis Horn <travis@travishorn.com>
2019-08-06 14:21:17 -0500

    Add basic serverless func support

---
+++ functions/protected.js
@@ -0,0 +1,22 @@
+export function handler(event, context, callback) {
+  const user = context.clientContext && context.clientContext.user;
+  const payload = JSON.parse(event.body);
+
+  if (!user) {
+    return callback(null, {
+      statusCode: 401,
+      body: "You must be signed in to call this function"
+    });
+  }
+
+  // Do something with the payload
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Request processed sucessfully",
+      email: user.email,
+      payload
+    })
+  });
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@ import netlifyIdentity from "netlify-identity-widget";
 
 const checkIdentityBtn = document.getElementById("checkIdentityBtn");
 const identityOutput = document.getElementById("identityOutput");
+const callFunctionBtn = document.getElementById("callFunctionBtn");
+const functionOutput = document.getElementById("functionOutput");
 
 netlifyIdentity.init();
 
@@ -13,4 +15,19 @@ const checkIdentity = () => {
   );
 };
 
+const callFunction = () => {
+  fetch("/.netlify/functions/protected", {
+    method: "POST",
+    body: JSON.stringify({ message: "Hello World" })
+  })
+    .then(res => res.json())
+    .then(data => {
+      functionOutput.innerText = data;
+    })
+    .catch(err => {
+      functionOutput.innerText = err;
+    });
+};
+
 checkIdentityBtn.addEventListener("click", checkIdentity);
+callFunctionBtn.addEventListener("click", callFunction);
